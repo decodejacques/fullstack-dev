530d8cf368ae0c06ac5fcb14c6204e3e875138f8
Vincent Sylvestre <vincent.sylvestre@hotmail.com>
2019-08-13 21:44:42 -0400

    I changed the app.jsx and the server.js so that it would show something that we can work on. You can access to 3 different 'endpoint' : signup,login,newitems. The css is halfway there but I have to stop for today

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Link, Router, NavLink } from "react-router-dom";
-import { connect } from "react-redux";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import Cart from "./Cart.jsx";
 import NewItem from "./NewItem.jsx";
 import Items from "./Items.jsx";
-import SignupForm from "./pages/Signup.jsx";
-import LoginForm from "./pages/Login.jsx";
-import "./main.css";
+import Signup from "./pages/Signup.jsx";
+import Login from "./pages/Login.jsx";
+import "./App.css";
 
 let renderSignupPage = () => {
-  return <div>signup form goes here</div>;
+  return (
+    <div>
+      <Signup />
+    </div>
+  );
 };
 let renderLoginPage = () => {
-  return <div>login form goes here</div>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 };
 
 let renderAllItems = () => {
@@ -41,78 +48,25 @@ let renderCart = () => {
   );
 };
 
-class UnconnectedApp extends Component {
+class App extends Component {
   render = () => {
-    console.log("this.props.signedIn", this.props.signedIn);
-    if (this.props.signedIn) {
-      return (
-        <div>
-          <BrowserRouter>
-            <div className="App">
-              <div className="App_Aside">
-                <div className="App_Form">
-                  <Route
-                    exact={true}
-                    path="/login-page"
-                    //   render={renderLoginPage}
-                    component={LoginForm}
-                  />
-                </div>
-              </div>
-            </div>
-          </BrowserRouter>
-          {/* <LoginForm /> */}
-        </div>
-      );
-    }
-
-    if (this.props.loggedIn) {
-      return (
-        <div>
-          <Items />
-        </div>
-      );
-    }
-
     return (
       <div>
-        <BrowserRouter>
-          <div>
-            <div className="App">
-              <div className="App_Aside">
-                <div className="App_Form">
-                  <Route
-                    exact={true}
-                    path="/signup-page"
-                    component={SignupForm}
-                    //   render={renderSignupPage}
-                  />
-                </div>
-              </div>
-            </div>
+        <Router>
+          <div className="App">
+            <div className="App__Aside" />
+            <div className="App__Form">
+              <div className="FormTitle" />
 
-            <Route exact={true} path="/all-items" render={renderAllItems} />
-            <Route
-              exact={true}
-              path="/all-items/:id"
-              render={renderItemDetails}
-            />
-            <Route exact={true} path="/new-item" render={renderNewItem} />
-            <Route exact={true} path="/cart" render={renderCart} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/Login" component={Login} />
+              <Route path="/Newitem" component={NewItem} />
+            </div>
           </div>
-        </BrowserRouter>
+        </Router>
       </div>
     );
   };
 }
-
-let mapStateToProps = state => {
-  return {
-    signedIn: state.signedIn,
-    loggedIn: state.loggedIn
-  };
-};
-
-let App = connect(mapStateToProps)(UnconnectedApp);
 
 export default App;
